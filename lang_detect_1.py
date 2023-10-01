3b18f95030f1ec9280b5294d165168f5705876ee
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-10-01 10:22:52 +0700

    Finish template language detector

--- lang_detect_1.py
+++ lang_detect_1.py
@@ -2,10 +2,10 @@
 from tkinter import *
 from langdetect import *
 from langcodes import *
-
+from tkinter import filedialog
 main_app = Tk()
 main_app.title("Language Detector")
-main_app.geometry("400x400")
+main_app.geometry("500x500")
 
 #Define detector function with probabilities
 def language_detect():
@@ -15,9 +15,30 @@ def language_detect():
         language_check = detect_langs(app_text.get(1.0, END))
         app_label.config(text = f"Identify Language is: {language_check}")
 
+#Define detector function 
+def check_language():
+    # ask the clients to choose a file
+    loca_file_name = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    # read the file content
+    with open(loca_file_name, 'r') as f:
+        content = f.read()
+        # check if the file is empty
+        if not content.strip():
+            app_label.config(text="The file is empty")
+        else:
+            # detect the language
+            language = detect_langs(content)
+            app_label.config(text=f"The language of the file is {language}")
+
 #Define box
-app_top = Label(main_app, text = "Input text:", height = 5, width = 10)
-app_top.pack(pady = 10)
+method2_label = Label(main_app, text = "Please select a file with txt extension", height = 5)
+my_button = Button(main_app, text = "Select File", command = check_language)
+
+app_top = Label(main_app, text = "Input text:", height = 3)
+
+method2_label.pack()
+my_button.pack()
+app_top.pack()
 
 app_text = Text(main_app, height = 10, width = 40)
 app_text.pack(pady = 10)
